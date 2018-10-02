acfaf2b25ad9d8c8752fab9e572669dca5cb01e2
skloibi <sebastian.kloibhofer@gmail.com>
2018-10-02 12:50:20 +0200

    Refine evaluation script and report differences in execution to console

--- eval-substratevm-hotspot.sh
+++ eval-substratevm-hotspot.sh
@@ -7,13 +7,18 @@ shift
 echo "Generating $FILES files"
 for i in `seq 1 $FILES`; do
   FILENAME="$PREFIX$i"
-  $SUBSTRATEVM_JAVA_HOME/bin/java -jar jbgenerator.jar -filename "$FILENAME" "$@" >/dev/null &&\
-  echo "file created" &&\
-  mx native-image "$FILENAME" &&\
-  echo "native image created" &&\
-  "./${FILENAME,,}" >"substratevm_out$i.txt" 2>&1 &&\
-  echo "native image executed" &&\
-  $HOTSPOT_JAVA_HOME/bin/java "$FILENAME" >"hotspot_out$i.txt" 2>&1 &&\
-  echo "HotSpot version executed" &&\
-  ! diff "substratevm_out$i.txt" "hotspot_out$i.txt" >"comp_out$i.txt" 2>&1
+  if $SUBSTRATEVM_JAVA_HOME/bin/java -jar jbgenerator.jar -filename "$FILENAME" "$@" >/dev/null; then
+    echo "file created"
+    mx native-image "$FILENAME" &&\
+    echo "native image created" &&\
+    "./${FILENAME,,}" >"substratevm_out$i.txt" 2>&1 &&\
+    echo "native image executed"
+
+    $HOTSPOT_JAVA_HOME/bin/java "$FILENAME" >"hotspot_out$i.txt" 2>&1 &&\
+    echo "HotSpot version executed"
+
+    if ! diff "substratevm_out$i.txt" "hotspot_out$i.txt" >"comp_out$i.txt" 2>&1; then
+      echo "Different outcome in file $FILENAME.class"
+    fi
+  fi
 done
