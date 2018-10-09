8e9d57fc368792e203c757a765c2329a443a24c9
skloibi <sebastian.kloibhofer@gmail.com>
2018-10-09 16:04:10 +0200

    Extends `StatementDSL` to enable generation of conditionals and loops

--- src/main/java/at/jku/ssw/java/bytecode/generator/utils/StatementDSL.java
+++ src/main/java/at/jku/ssw/java/bytecode/generator/utils/StatementDSL.java
@@ -11,17 +11,70 @@ public class StatementDSL {
         public static String Return(String value) {
             return String.format(RETURN, value);
         }
+
+    }
+
+    public static class Assignments<T> {
+        public static final String LOCAL_VAR = "%s %s = %s";
+
+        private final T value;
+
+        private Assignments(T value) {
+            this.value = value;
+        }
+
+        public static <T> Assignments<T> assign(T value) {
+            return new Assignments<>(value);
+        }
+
+        public String toLocalVar(Class<?> type, String name) {
+            return String.format(LOCAL_VAR, type.getSimpleName(), name, value);
+        }
     }
 
     public static class Patterns {
         public static final String SYSTEM_OUT_PRINTLN = "System.out.println(%s)";
         public static final String AS_STRING = "\"%s\"";
-        public static final String ASSIGN = "%s = %s";
-        public static final String STATEMENT = "%s;";
+        public static final String STATEMENT = "%s; ";
         public static final String SUBTRACT = "%s - %s";
         public static final String CALL_NO_PARAMS = "%s()";
         public static final String IN_PAR = "(%s)";
         public static final String RETURN = "return %s;";
+        public static final String INCR = "%s++";
+        public static final String DECR = "%s--";
+        public static final String GT = "%s > %s";
+        public static final String LT = "%s < %s";
+    }
+
+    public static class Blocks {
+        public static final String IF = "if (%s) { ";
+        public static final String WHILE = "while (%s) { ";
+        public static final String Do = "do { ";
+        public static final String BlockEnd = "} ";
+        public static final String DOWHILE = "} while (%s);";
+        public static final String Else = "} else { ";
+        public static final String ELSEIF = "} else if (%s) { ";
+        public static final String FOR = "for (%s; %s; %s) { ";
+
+        public static String If(String condition) {
+            return String.format(IF, condition);
+        }
+
+        public static String While(String condition) {
+            return String.format(WHILE, condition);
+        }
+
+        public static String DoWhile(String condition) {
+            return String.format(DOWHILE, condition);
+        }
+
+        public static String ElseIf(String condition) {
+            return String.format(ELSEIF, condition);
+        }
+
+        public static String For(String init, String cond, String chang) {
+            return String.format(FOR, init, cond, chang);
+        }
     }
 
     public static String spaced(String... words) {
@@ -40,9 +93,6 @@ public class StatementDSL {
         return String.format(AS_STRING, str);
     }
 
-    public static String assign(String dest, String src) {
-        return String.format(ASSIGN, dest, src);
-    }
 
     public static String subtract(String a, String b) {
         return String.format(SUBTRACT, a, b);
@@ -60,4 +110,19 @@ public class StatementDSL {
         return String.format(IN_PAR, str);
     }
 
+    public static String incr(String str) {
+        return String.format(INCR, str);
+    }
+
+    public static String decr(String str) {
+        return String.format(DECR, str);
+    }
+
+    public static String gt(Object a, Object b) {
+        return String.format(GT, a, b);
+    }
+
+    public static String lt(Object a, Object b) {
+        return String.format(LT, a, b);
+    }
 }
