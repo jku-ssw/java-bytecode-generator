e9249a70420ee2300809fa8964584089af9f05f8
skloibi <sebastian.kloibhofer@gmail.com>
2018-10-09 11:14:28 +0200

    Refactor `RandomizerTest`

--- src/test/java/at/jku/ssw/java/bytecode/generator/utils/RandomizerTest.java
+++ src/test/java/at/jku/ssw/java/bytecode/generator/utils/RandomizerTest.java
@@ -3,6 +3,7 @@ package at.jku.ssw.java.bytecode.generator.utils;
 import org.junit.jupiter.api.Test;
 
 import java.util.Optional;
+import java.util.function.Supplier;
 
 import static at.jku.ssw.java.bytecode.generator.utils.Randomizer.shuffledUntilNotNull;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -12,28 +13,36 @@ import static org.hamcrest.Matchers.isOneOf;
 public class RandomizerTest {
 
     @Test
-    public void testShuffleUntilNonNullForNullSequences() {
-        assertThat(shuffledUntilNotNull(() -> null), is(Optional.empty()));
+    public void testShuffledUntilNonNullForNullSequences() {
+        assertThat(shuffledUntilNotNull(() -> null), is(opt(null)));
     }
 
     @Test
-    public void testShuffleUntilNonNullForEmptySequences() {
-        assertThat(shuffledUntilNotNull(), is(Optional.empty()));
+    public void testShuffledUntilNonNullForEmptySequences() {
+        assertThat(shuffledUntilNotNull(), is(opt(null)));
     }
 
     @Test
-    public void testShuffleUntilNonNullForNonNullValue() {
-        assertThat(shuffledUntilNotNull(() -> 0), is(Optional.of(0)));
+    public void testShuffledUntilNonNullForNonNullValue() {
+        assertThat(shuffledUntilNotNull(ret(0)), is(opt(0)));
     }
 
     @Test
-    public void testShuffleUntilNonNullForNonNullValues() {
-        assertThat(shuffledUntilNotNull(() -> 0, () -> 1), is(isOneOf(Optional.of(0), Optional.of(1))));
+    public void testShuffledUntilNonNullForNonNullValues() {
+        assertThat(shuffledUntilNotNull(ret(0), ret(1)), is(isOneOf(opt(0), opt(1))));
     }
 
     @Test
-    public void testShuffleUntilNonNullForMixedValues() {
-        assertThat(Randomizer.<Object>shuffledUntilNotNull(() -> 0, () -> null), is(Optional.of(0)));
-        assertThat(Randomizer.<Object>shuffledUntilNotNull(() -> null, () -> 1, () -> null), is(Optional.of(1)));
+    public void testShuffledUntilNonNullForMixedValues() {
+        assertThat(Randomizer.<Object>shuffledUntilNotNull(ret(0), ret(null)), is(opt(0)));
+        assertThat(Randomizer.<Object>shuffledUntilNotNull(ret(null), ret(1), ret(null)), is(opt(1)));
+    }
+
+    private <T> Supplier<T> ret(T value) {
+        return () -> value;
+    }
+
+    private <T> Optional<T> opt(T value) {
+        return Optional.ofNullable(value);
     }
 }
