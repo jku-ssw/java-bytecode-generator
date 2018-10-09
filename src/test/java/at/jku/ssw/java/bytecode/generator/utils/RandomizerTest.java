57e0cecec60b4546edfcb55e721aa54767a2793b
skloibi <sebastian.kloibhofer@gmail.com>
2018-10-09 12:04:16 +0200

    Test side-effect behaviour in `RandomizerTest`

--- src/test/java/at/jku/ssw/java/bytecode/generator/utils/RandomizerTest.java
+++ src/test/java/at/jku/ssw/java/bytecode/generator/utils/RandomizerTest.java
@@ -2,7 +2,9 @@ package at.jku.ssw.java.bytecode.generator.utils;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.HashSet;
 import java.util.Optional;
+import java.util.Set;
 import java.util.function.Supplier;
 
 import static at.jku.ssw.java.bytecode.generator.utils.Randomizer.shuffledUntilNotNull;
@@ -28,6 +30,7 @@ public class RandomizerTest {
     }
 
     @Test
+    @SuppressWarnings("unchecked")
     public void testShuffledUntilNonNullForNonNullValues() {
         assertThat(shuffledUntilNotNull(ret(0), ret(1)), is(isOneOf(opt(0), opt(1))));
     }
@@ -36,6 +39,30 @@ public class RandomizerTest {
     public void testShuffledUntilNonNullForMixedValues() {
         assertThat(Randomizer.<Object>shuffledUntilNotNull(ret(0), ret(null)), is(opt(0)));
         assertThat(Randomizer.<Object>shuffledUntilNotNull(ret(null), ret(1), ret(null)), is(opt(1)));
+    }
+
+    @Test
+    @SuppressWarnings("unchecked")
+    public void testShuffledUntilNonNullForStatefulProcedures() {
+        Set<Integer> modifications = new HashSet<>();
+
+        Supplier[] suppliers = new Supplier[] {
+                () -> {
+                    modifications.add(0);
+                    return 0;
+                },
+                () -> {
+                    modifications.add(1);
+                    return 1;
+                },
+                () -> {
+                    modifications.add(2);
+                    return 2;
+                }
+        };
+
+        assertThat(Randomizer.shuffledUntilNotNull(suppliers), is(isOneOf(opt(0), opt(1), opt(2))));
+        assertThat(modifications.size(), is(1));
     }
 
     private <T> Supplier<T> ret(T value) {
