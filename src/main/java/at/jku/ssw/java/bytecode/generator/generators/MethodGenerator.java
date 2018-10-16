76a76a33ae6fee3abf820c9cfda882ac24da7846
skloibi <sebastian.kloibhofer@gmail.com>
2018-10-16 11:03:53 +0200

    Fix stack map frame type errors by forcing rebuild after method creation

--- src/main/java/at/jku/ssw/java/bytecode/generator/generators/MethodGenerator.java
+++ src/main/java/at/jku/ssw/java/bytecode/generator/generators/MethodGenerator.java
@@ -7,6 +7,7 @@ import at.jku.ssw.java.bytecode.generator.utils.ParamWrapper;
 import at.jku.ssw.java.bytecode.generator.utils.RandomSupplier;
 import at.jku.ssw.java.bytecode.generator.utils.Randomizer;
 import javassist.*;
+import javassist.bytecode.BadBytecode;
 
 import java.util.Date;
 import java.util.List;
@@ -134,6 +135,12 @@ class MethodGenerator extends MethodCaller {
                     }
                 }
         );
+
+        try {
+            ctMethod.getMethodInfo().rebuildStackMap(ClassPool.getDefault());
+        } catch (BadBytecode badBytecode) {
+            badBytecode.printStackTrace();
+        }
     }
 
     //===============================================Method Calling=====================================================
