cdfe275ac7616e2e772b120f2466ecba55173974
skloibi <sebastian.kloibhofer@gmail.com>
2018-11-06 09:58:36 +0100

    Add more test cases for `FieldVarType`

--- src/test/java/at/jku/ssw/java/bytecode/generator/utils/FieldVarTypeTest.java
+++ src/test/java/at/jku/ssw/java/bytecode/generator/utils/FieldVarTypeTest.java
@@ -1,5 +1,6 @@
 package at.jku.ssw.java.bytecode.generator.utils;
 
+import at.jku.ssw.java.bytecode.generator.logger.FieldVarLogger;
 import at.jku.ssw.java.bytecode.generator.types.FieldVarType;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
@@ -15,48 +16,8 @@ import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 public class FieldVarTypeTest {
 
-    private static Stream<Arguments> compatibleTypesProvider() {
-        return Stream.of(
-                arguments(BYTE, BYTE, true),
-                arguments(SHORT, BYTE, true),
-                arguments(SHORT, SHORT, true),
-                arguments(INT, BYTE, true),
-                arguments(INT, SHORT, true),
-                arguments(INT, INT, true),
-                arguments(LONG, INT, true),
-                arguments(DOUBLE, FLOAT, true),
-                arguments(STRING, STRING, true),
-                arguments(DATE, DATE, true)
-        );
-    }
-
-    private static Stream<Arguments> incompatibleTypesProvider() {
-        return Stream.of(
-                arguments(BYTE, SHORT, false),
-                arguments(SHORT, INT, false),
-                arguments(INT, STRING, false),
-                arguments(FLOAT, DOUBLE, false),
-                arguments(DOUBLE, INT, false),
-                arguments(STRING, DATE, false),
-                arguments(VOID, DATE, false),
-                arguments(VOID, VOID, false)
-        );
-    }
-
-    private static Stream<Arguments> arrayTypeProvider() {
-        return Stream.of(
-                arguments(classType(String.class), 1, arrayType(String[].class, STRING)),
-                arguments(classType(Date.class), 3, arrayType(Date[][][].class, DATE)),
-                arguments(FieldVarType.BYTE, 19, arrayType(byte[][][][][][][][][][][][][][][][][][][].class, BYTE)),
-                arguments(FieldVarType.SHORT, 2, arrayType(short[][].class, SHORT)),
-                arguments(FieldVarType.INT, 10, arrayType(int[][][][][][][][][][].class, INT)),
-                arguments(FieldVarType.LONG, 5, arrayType(long[][][][][].class, LONG)),
-                arguments(FieldVarType.FLOAT, 2, arrayType(float[][].class, FLOAT)),
-                arguments(FieldVarType.DOUBLE, 4, arrayType(double[][][][].class, DOUBLE)),
-                arguments(FieldVarType.CHAR, 7, arrayType(char[][][][][][][].class, CHAR)),
-                arguments(FieldVarType.BOOLEAN, 1, arrayType(boolean[].class, BOOLEAN))
-        );
-    }
+    //-------------------------------------------------------------------------
+    // region Test cases
 
     @ParameterizedTest(name = "''{0}'' is assignable from ''{1}''")
     @MethodSource("compatibleTypesProvider")
@@ -77,18 +38,96 @@ public class FieldVarTypeTest {
         assertThat(arrayTypeOf(componentType, dim), is(arrayType));
     }
 
+    @ParameterizedTest(name = "resulting type of ''{0}'' being accessed at dimension ''{2}'' is ''{3}''")
+    @MethodSource("arrayTypeAndAccessProvider")
+    public void testResultingTypeOf(Class<?> arrayType, FieldVarType<?> inner, int nParams, FieldVarType<?> expected) {
+        assertThat(
+                resultingTypeOf(arrayOf(arrayType, inner), nParams),
+                is(expected)
+        );
+    }
+
+    // endregion
+    //-------------------------------------------------------------------------
+    // region Test utilities
+
+    private static FieldVarLogger arrayOf(Class<?> type, FieldVarType<?> componentType) {
+        return new FieldVarLogger("", "", 0, arrayType(type, componentType), false, false);
+    }
+
     private static <T> FieldVarType<T> classType(Class<T> type) {
-        return FieldVarType.of(type);
+        return refTypeOf(type);
     }
 
     private static <T> FieldVarType<T> arrayType(Class<T> type, FieldVarType<?> componentType) {
         assert type.isArray();
 
-        return FieldVarType.of(
-                type,
-                ClassUtils.dimensions(type),
-                componentType
+        return arrayTypeOf(type, componentType);
+    }
+
+    // endregion
+    //-------------------------------------------------------------------------
+    // region Argument providers
+
+    private static Stream<Arguments> compatibleTypesProvider() {
+        return Stream.of(
+                arguments(BYTE, BYTE),
+                arguments(SHORT, BYTE),
+                arguments(SHORT, SHORT),
+                arguments(INT, BYTE),
+                arguments(INT, SHORT),
+                arguments(INT, INT),
+                arguments(LONG, INT),
+                arguments(DOUBLE, FLOAT),
+                arguments(STRING, STRING),
+                arguments(DATE, DATE),
+                arguments(OBJECT, DATE),
+                arguments(OBJECT, STRING),
+                arguments(OBJECT, OBJECT),
+                arguments(OBJECT, arrayType(Object[].class, OBJECT))
         );
     }
+
+    private static Stream<Arguments> incompatibleTypesProvider() {
+        return Stream.of(
+                arguments(BYTE, SHORT),
+                arguments(SHORT, INT),
+                arguments(INT, STRING),
+                arguments(FLOAT, DOUBLE),
+                arguments(DOUBLE, INT),
+                arguments(STRING, DATE),
+                arguments(VOID, DATE),
+                arguments(VOID, VOID),
+                arguments(STRING, OBJECT),
+                arguments(OBJECT, INT)
+        );
+    }
+
+    private static Stream<Arguments> arrayTypeProvider() {
+        return Stream.of(
+                arguments(classType(String.class), 1, arrayType(String[].class, STRING)),
+                arguments(classType(Date.class), 3, arrayType(Date[][][].class, DATE)),
+                arguments(BYTE, 19, arrayType(byte[][][][][][][][][][][][][][][][][][][].class, BYTE)),
+                arguments(SHORT, 2, arrayType(short[][].class, SHORT)),
+                arguments(INT, 10, arrayType(int[][][][][][][][][][].class, INT)),
+                arguments(LONG, 5, arrayType(long[][][][][].class, LONG)),
+                arguments(FLOAT, 2, arrayType(float[][].class, FLOAT)),
+                arguments(DOUBLE, 4, arrayType(double[][][][].class, DOUBLE)),
+                arguments(CHAR, 7, arrayType(char[][][][][][][].class, CHAR)),
+                arguments(BOOLEAN, 1, arrayType(boolean[].class, BOOLEAN)),
+                arguments(OBJECT, 7, arrayType(Object[][][][][][][].class, OBJECT))
+        );
+    }
+
+    private static Stream<Arguments> arrayTypeAndAccessProvider() {
+        return Stream.of(
+                arguments(Object[][][][].class, OBJECT, 1, arrayTypeOf(Object[][][].class, OBJECT)),
+                arguments(int[].class, INT, 1, INT),
+                arguments(boolean[][][].class, INT, 2, arrayTypeOf(boolean[].class, BOOLEAN))
+        );
+    }
+
+    // endregion
+    //-------------------------------------------------------------------------
 
 }
