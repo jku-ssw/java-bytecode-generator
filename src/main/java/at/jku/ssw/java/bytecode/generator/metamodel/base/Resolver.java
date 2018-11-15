02fd5381ab1033ba5eba9be1e6d20e7c15502214
skloibi <sebastian.kloibhofer@gmail.com>
2018-11-15 13:38:34 +0100

    Define `Resolver` interface that converts expressions to code This should be the interface if the generator is ever adapted to directly produce Java bytecode i/o producing Javassist Strings

---
+++ src/main/java/at/jku/ssw/java/bytecode/generator/metamodel/base/Resolver.java
@@ -0,0 +1,83 @@
+package at.jku.ssw.java.bytecode.generator.metamodel.base;
+
+import at.jku.ssw.java.bytecode.generator.logger.FieldVarLogger;
+import at.jku.ssw.java.bytecode.generator.metamodel.base.constants.*;
+import at.jku.ssw.java.bytecode.generator.utils.ErrorUtils;
+
+/**
+ * Generic interface for resolvers that interpret generated expressions.
+ *
+ * @param <T> The type of result that this resolver produces
+ */
+public interface Resolver<T> {
+    default <U> T resolve(Expression<U> expression) {
+        assert expression != null;
+
+        if (expression instanceof Constant)
+            return resolve((Constant<U>) expression);
+        if (expression instanceof TypeIdentifier)
+            return resolve((TypeIdentifier<U>) expression);
+        if (expression instanceof ArrayInit)
+            return resolve((ArrayInit<U>) expression);
+        if (expression instanceof ConstructorCall)
+            return resolve((ConstructorCall<U>) expression);
+        if (expression instanceof FieldVarLogger)
+            return resolve((FieldVarLogger<T>) expression);
+
+        throw ErrorUtils.shouldNotReachHere("Unexpected expression " + expression.getClass());
+    }
+
+    default <U> T resolve(Constant<U> constant) {
+        if (constant instanceof ByteConstant)
+            return resolve((ByteConstant) constant);
+        if (constant instanceof ShortConstant)
+            return resolve((ShortConstant) constant);
+        if (constant instanceof IntConstant)
+            return resolve((IntConstant) constant);
+        if (constant instanceof LongConstant)
+            return resolve((LongConstant) constant);
+        if (constant instanceof FloatConstant)
+            return resolve((FloatConstant) constant);
+        if (constant instanceof DoubleConstant)
+            return resolve((DoubleConstant) constant);
+        if (constant instanceof BooleanConstant)
+            return resolve((BooleanConstant) constant);
+        if (constant instanceof StringConstant)
+            return resolve((StringConstant) constant);
+        if (constant instanceof NullConstant)
+            return resolve((NullConstant<U>) constant);
+        if (constant instanceof CharConstant)
+            return resolve((CharConstant) constant);
+
+        throw ErrorUtils.shouldNotReachHere("Unexpected constant " + constant.getClass());
+    }
+
+    T resolve(ByteConstant constant);
+
+    T resolve(ShortConstant constant);
+
+    T resolve(IntConstant constant);
+
+    T resolve(LongConstant constant);
+
+    T resolve(FloatConstant constant);
+
+    T resolve(DoubleConstant constant);
+
+    T resolve(BooleanConstant constant);
+
+    T resolve(StringConstant constant);
+
+    <U> T resolve(NullConstant<U> constant);
+
+    <U> T resolve(CharConstant constant);
+
+    <U> T resolve(TypeIdentifier<U> typeIdentifier);
+
+    <U> T resolve(ArrayInit<U> arrayInit);
+
+    <U> T resolve(ConstructorCall<U> constructorCall);
+
+    <U> T resolve(FieldVarLogger<U> fieldVarLogger);
+
+}
