02f92b1d39ecd688ff340866bbc0bc1023bb665b
skloibi <sebastian.kloibhofer@gmail.com>
2018-11-15 13:36:59 +0100

    Add helper to combine builders and their resolved expressions

---
+++ src/main/java/at/jku/ssw/java/bytecode/generator/metamodel/base/ResolvedBuilder.java
@@ -0,0 +1,34 @@
+package at.jku.ssw.java.bytecode.generator.metamodel.base;
+
+import java.util.List;
+
+/**
+ * Utility to combine {@link Builder} instances with corresponding
+ * expressions that may be used to invoke it (e.g. constants, variable access
+ * or other expressions).
+ *
+ * @param <T>
+ */
+public class ResolvedBuilder<T> {
+    /**
+     * The builder instance that is used.
+     */
+    public final Builder<T> builder;
+
+    /**
+     * The actual parameter expressions for the builder.
+     */
+    public final List<Expression<?>> params;
+
+    /**
+     * Creates a new resolved builder for the given builder and the given
+     * parameter expressions that are used to generate the final expression.
+     *
+     * @param builder The builder instance
+     * @param params  The list of parameter expressions
+     */
+    public ResolvedBuilder(Builder<T> builder, List<Expression<?>> params) {
+        this.builder = builder;
+        this.params = params;
+    }
+}
