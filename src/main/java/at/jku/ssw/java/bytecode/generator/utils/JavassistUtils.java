59e464e04687958f1fb19e1e3890473d434d50eb
skloibi <sebastian.kloibhofer@gmail.com>
2018-11-22 13:01:28 +0100

    Infer `CtClass` instance from `MetaType`s in `JavassistUtils.toCtClass`

--- src/main/java/at/jku/ssw/java/bytecode/generator/utils/JavassistUtils.java
+++ src/main/java/at/jku/ssw/java/bytecode/generator/utils/JavassistUtils.java
@@ -1,5 +1,6 @@
 package at.jku.ssw.java.bytecode.generator.utils;
 
+import at.jku.ssw.java.bytecode.generator.types.base.MetaType;
 import javassist.ClassPool;
 import javassist.CtClass;
 import javassist.NotFoundException;
@@ -13,15 +14,54 @@ public class JavassistUtils {
      * Determines and returns the {@link javassist.CtClass} instance
      * corresponding to the given class name.
      *
-     * @param clazz The Java class type instance that should be mapped
+     * @param className The name of the class that should be mapped
      * @return the class type corresponding to the class name
+     */
+    public static CtClass toCtClass(String className) {
+        assert className != null;
+        try {
+            return ClassPool.getDefault().get(className);
+        } catch (NotFoundException e) {
+            throw ErrorUtils.shouldNotReachHere("Could not generate CtClass for '" + className + "'");
+        }
+    }
+
+    /**
+     * @see #toCtClass(String)
      */
     public static CtClass toCtClass(Class<?> clazz) {
         assert clazz != null;
-        try {
-            return ClassPool.getDefault().get(clazz.getCanonicalName());
-        } catch (NotFoundException e) {
-            throw new AssertionError(e);
+        return toCtClass(clazz.getCanonicalName());
+    }
+
+    /**
+     * @see #toCtClass(String)
+     */
+    public static CtClass toCtClass(MetaType<?> type) {
+        switch (type.kind()) {
+            case BYTE:
+                return CtClass.byteType;
+            case SHORT:
+                return CtClass.shortType;
+            case RINT:
+            case INT:
+                return CtClass.intType;
+            case LONG:
+                return CtClass.longType;
+            case FLOAT:
+                return CtClass.floatType;
+            case DOUBLE:
+                return CtClass.doubleType;
+            case BOOLEAN:
+                return CtClass.booleanType;
+            case CHAR:
+                return CtClass.charType;
+            case VOID:
+                return CtClass.voidType;
+            default:
+                break;
         }
+
+        return toCtClass(type.descriptor());
     }
 }
