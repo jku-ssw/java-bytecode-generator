cf7504f67d1fd8a37e230594d5f35fa1011eb68a
skloibi <sebastian.kloibhofer@gmail.com>
2018-11-26 09:53:57 +0100

    Add NOP `Expression` implementation

--- src/main/java/at/jku/ssw/java/bytecode/generator/metamodel/base/Expression.java
+++ src/main/java/at/jku/ssw/java/bytecode/generator/metamodel/base/Expression.java
@@ -1,6 +1,7 @@
 package at.jku.ssw.java.bytecode.generator.metamodel.base;
 
 import at.jku.ssw.java.bytecode.generator.types.base.MetaType;
+import at.jku.ssw.java.bytecode.generator.types.base.VoidType;
 
 /**
  * Describes an expression that eventually evaluates to some result type.
@@ -17,4 +18,20 @@ public interface Expression<T> {
      * @return the type corresponding to the evaluated type
      */
     MetaType<? extends T> type();
+
+    /**
+     * Expression that represents "NOP" - i.e. an empty expression.
+     */
+    enum NOP implements Expression<Void> {
+
+        NOP;
+
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        public MetaType<Void> type() {
+            return VoidType.VOID;
+        }
+    }
 }
